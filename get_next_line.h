3746a5573abff45cba07384e076d282217173e4f
ikermusa <ikermusatadi123@gmail.com>
2024-10-12 13:03:36 +0200

    utils y libreria listo

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imusatad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 09:28:45 by imusatad          #+#    #+#             */
+/*   Updated: 2024/10/12 09:28:49 by imusatad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imusatad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 09:29:01 by imusatad          #+#    #+#             */
+/*   Updated: 2024/10/12 09:29:11 by imusatad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len_s1;
+	size_t	len_s2;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = (char *)malloc(len_s1 + len_s2 + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len_s1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (i < len_s1 + len_s2)
+	{
+		str[i] = s2[i - len_s1];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*copy;
+	int		i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	copy = (char *)malloc((i + 1) * sizeof(char));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		copy[i] = s[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subcadena;
+	size_t	cadlen;
+
+	if (start >= ft_strlen(s))
+	{
+		subcadena = (char *)malloc(sizeof(char) * 1);
+		if (!subcadena)
+			return (NULL);
+		subcadena[0] = '\0';
+		return (subcadena);
+	}
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	subcadena = (char *)malloc((len + 1) * sizeof(char));
+	if (!subcadena)
+		return (NULL);
+	while (cadlen < len && s[start + cadlen] != '\0')
+	{
+		subcadena[cadlen] = s[start + cadlen];
+		cadlen++;
+	}
+	subcadena[cadlen] = '\0';
+	return (subcadena);
+}
